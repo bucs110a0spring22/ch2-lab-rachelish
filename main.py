791008a4c145ba4a44393e7ccccf57d5b9d17952
Rachel E E <62678193+rachelish@users.noreply.github.com>
2022-02-14 16:35:33 +0000

    Finished the lab

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+#Part B
+import random 
+list = ["3", 1, 1.002, "floop", 4] 
+x = random.choice(list) 
+print(x)
 
 #Part A
 weeks = 16
@@ -5,6 +10,9 @@ classes = 5
 tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
+print(weeks, type(weeks))
+print(classes, type(classes))
+print(tuition, type(tuition))
+print(cost_per_week, type(cost_per_week))
 
 
-#Part B
